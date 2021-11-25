79a6c1ce66dd58867aaa16a8905be511a1f900e1
Steve Brown <steve.brown+github@brownserve.co.uk>
2021-11-25 00:47:52 +0000

    Add the wait-forconvergence function

---
+++ tests/functions.ps1
@@ -0,0 +1,65 @@
+# Waits until Puppet has applied idempotently
+function Wait-UntilConvergence
+{
+    [CmdletBinding()]
+    param(
+        [string] $ComputerName,
+        [string] $PuppetServer,
+        [string] $PuppetDBPort,
+        # Timeout in mins. Default to 300min (5h)
+        [int] $Timeout = 30
+    )
+
+    Write-Verbose 'Checking Puppet state...'
+    $startTime = [datetime]::UtcNow
+    $watch = [System.Diagnostics.Stopwatch]::StartNew()
+    try
+    {
+        $LastReportSummary = @{ latest_report_status = 'AnyStringWillDo' }
+        while ($LastReportSummary.latest_report_status -ne 'unchanged' -or [datetime]$LastReportSummary.report_timestamp -lt $startTime)
+        {
+
+            if ($watch.Elapsed.TotalMinutes -gt $Timeout)
+            {
+                throw "Timed out waiting for $ComputerName to reach desired state after $Timeout minutes..."
+            }
+
+            Start-Sleep 90
+
+            try
+            {
+                $LastReportSummary = Invoke-RestMethod "http://${PuppetServer}:${PuppetDBPort}/pdb/query/v4/nodes/${ComputerName}"
+            }
+            catch
+            {
+                # Print any error as a warning - we want to continue in case PuppetDB isn't ready yet
+                $errorFromPuppetDb = $_.ErrorDetails.Message
+                $errorMessage = $_.Exception.Message
+            }
+            # We need to use another try catch block to avoid having the ConvertFrom-Json error message bomb out the whole script :(
+            try
+            {
+                $errorFromPuppetDb = ($errorFromPuppetDb | ConvertFrom-Json -ErrorAction SilentlyContinue).error
+                # If we can ConvertFrom-Json the error, then we know it's a PuppetDB error 
+                Write-Warning $errorFromPuppetDb
+            }
+            catch
+            {
+                # Otherwise it's a generic error
+                Write-Warning $errorMessage
+            }
+        }
+
+        Write-Host "Reached desired state."
+        Write-Debug @"
+`$LastReportSummary.latest_report_status=$($LastReportSummary.latest_report_status)
+`$LastReportSummary.report_timestamp=$($LastReportSummary.report_timestamp)
+`$starttime=${starttime}
+"@
+
+    }
+    finally
+    {
+        $watch.Stop()
+    }
+}
